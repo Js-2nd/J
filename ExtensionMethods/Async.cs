46442713488f4736785e2459a824c224b0083fe0
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-08-04 20:21:15 +0900

    Async

---
+++ ExtensionMethods/Async.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UniRx;
+
+namespace J
+{
+	public static partial class ExtensionMethods
+	{
+		public static IObservable<T> ToObservable<T>(this IAsyncEnumerator<T> @this)
+		{
+			Subject<T> subject = new Subject<T>();
+			Task.Run(async () =>
+			{
+				while (await @this.MoveNext())
+				{
+					subject.OnNext(@this.Current);
+				}
+				subject.OnCompleted();
+			});
+			return subject;
+		}
+	}
+}
