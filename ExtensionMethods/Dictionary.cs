cf4b70b299b8111a764dd4093a9fda637d0933ec
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-10-11 17:09:40 +0900

    ExtensionMethods/Dictionary

--- ExtensionMethods/Dictionary.cs
+++ ExtensionMethods/Dictionary.cs
@@ -2,6 +2,7 @@
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
 
 	public static partial class ExtensionMethods
 	{
@@ -12,13 +13,10 @@
 				value = @default;
 			return value;
 		}
-		public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue @default = default(TValue))
-		{
-			TValue value;
-			if (@this.TryGetValue(key, out value) == false)
-				value = @default;
-			return value;
-		}
+		public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue @default = default(TValue)) =>
+			new ReadOnlyDictionary<TKey, TValue>(@this).StaticCast<IReadOnlyDictionary<TKey, TValue>>().GetOrDefault(key, @default);
+		public static TValue GetOrDefault<TKey, TValue>(this ReadOnlyDictionary<TKey, TValue> @this, TKey key, TValue @default = default(TValue)) =>
+			@this.StaticCast<IReadOnlyDictionary<TKey, TValue>>().GetOrDefault(key, @default);
 		public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> @this, TKey key, TValue @default = default(TValue)) =>
 			@this.StaticCast<IReadOnlyDictionary<TKey, TValue>>().GetOrDefault(key, @default);
 
@@ -29,13 +27,10 @@
 				value = factory(key);
 			return value;
 		}
-		public static TValue GetOrDefault<TKey, TValue, TNewValue>(this IDictionary<TKey, TValue> @this, TKey key, Func<TKey, TNewValue> factory) where TNewValue : TValue
-		{
-			TValue value;
-			if (@this.TryGetValue(key, out value) == false)
-				value = factory(key);
-			return value;
-		}
+		public static TValue GetOrDefault<TKey, TValue, TNewValue>(this IDictionary<TKey, TValue> @this, TKey key, Func<TKey, TNewValue> factory) where TNewValue : TValue =>
+			new ReadOnlyDictionary<TKey, TValue>(@this).StaticCast<IReadOnlyDictionary<TKey, TValue>>().GetOrDefault(key, factory);
+		public static TValue GetOrDefault<TKey, TValue, TNewValue>(this ReadOnlyDictionary<TKey, TValue> @this, TKey key, Func<TKey, TNewValue> factory) where TNewValue : TValue =>
+			@this.StaticCast<IReadOnlyDictionary<TKey, TValue>>().GetOrDefault(key, factory);
 		public static TValue GetOrDefault<TKey, TValue, TNewValue>(this Dictionary<TKey, TValue> @this, TKey key, Func<TKey, TNewValue> factory) where TNewValue : TValue =>
 			@this.StaticCast<IReadOnlyDictionary<TKey, TValue>>().GetOrDefault(key, factory);
 
