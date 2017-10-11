b58af74e147429a21485f8bda87146bf84e4a32e
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-10-11 16:36:24 +0900

    ExtensionMethods/Dictionary

--- ExtensionMethods/Dictionary.cs
+++ ExtensionMethods/Dictionary.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace J
+﻿namespace J
 {
+	using System;
+	using System.Collections.Generic;
+
 	public static partial class ExtensionMethods
 	{
+		public static TValue GetOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> @this, TKey key, TValue @default = default(TValue))
+		{
+			TValue value;
+			if (@this.TryGetValue(key, out value) == false)
+				value = @default;
+			return value;
+		}
 		public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue @default = default(TValue))
 		{
 			TValue value;
@@ -12,7 +19,16 @@ namespace J
 				value = @default;
 			return value;
 		}
+		public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> @this, TKey key, TValue @default = default(TValue)) =>
+			@this.StaticCast<IReadOnlyDictionary<TKey, TValue>>().GetOrDefault(key, @default);
 
+		public static TValue GetOrDefault<TKey, TValue, TNewValue>(this IReadOnlyDictionary<TKey, TValue> @this, TKey key, Func<TKey, TNewValue> factory) where TNewValue : TValue
+		{
+			TValue value;
+			if (@this.TryGetValue(key, out value) == false)
+				value = factory(key);
+			return value;
+		}
 		public static TValue GetOrDefault<TKey, TValue, TNewValue>(this IDictionary<TKey, TValue> @this, TKey key, Func<TKey, TNewValue> factory) where TNewValue : TValue
 		{
 			TValue value;
@@ -20,6 +36,8 @@ namespace J
 				value = factory(key);
 			return value;
 		}
+		public static TValue GetOrDefault<TKey, TValue, TNewValue>(this Dictionary<TKey, TValue> @this, TKey key, Func<TKey, TNewValue> factory) where TNewValue : TValue =>
+			@this.StaticCast<IReadOnlyDictionary<TKey, TValue>>().GetOrDefault(key, factory);
 
 		public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue @default = default(TValue))
 		{
