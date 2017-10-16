e42b95d4856308f67145d71d6a2faadea3a5896c
Js <JimmyLoveSiren@users.noreply.github.com>
2017-10-17 00:52:39 +0900

    AnonymousObservable

---
+++ AnonymousObservable.cs
@@ -0,0 +1,16 @@
+﻿namespace J
+{
+	using System;
+
+	public class AnonymousObservable<T> : IObservable<T>
+	{
+		readonly Func<IObserver<T>, IDisposable> onSubscribe;
+
+		public AnonymousObservable(Func<IObserver<T>, IDisposable> onSubscribe)
+		{
+			this.onSubscribe = onSubscribe;
+		}
+
+		public IDisposable Subscribe(IObserver<T> observer) => onSubscribe(observer);
+	}
+}
