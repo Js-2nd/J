1c164bac3eeb747a8eab93afcb10c24ec45a1202
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-10-20 18:11:05 +0900

    WithProgress

---
+++ ExtensionMethods/Progress.cs
@@ -0,0 +1,23 @@
+﻿namespace J
+{
+	using System;
+	using UnityEngine;
+
+	public static partial class ExtensionMethods
+	{
+		public static void ReportWithoutException<T>(this IProgress<T> @this, T value, bool logError = true)
+		{
+			try
+			{
+				@this.Report(value);
+			}
+			catch (Exception ex)
+			{
+				if (logError)
+				{
+					Debug.LogError(ex);
+				}
+			}
+		}
+	}
+}
