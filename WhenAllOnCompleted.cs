ec6d33f48582602d274c84997987e941fc7e80ce
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-10-20 16:48:09 +0900

    WhenAllOnCompleted

---
+++ WhenAllOnCompleted.cs
@@ -0,0 +1,29 @@
+﻿namespace J
+{
+	using System;
+	using System.Collections.Generic;
+	using UniRx;
+
+	public static partial class ExtensionMethods
+	{
+		public static IObservable<Unit> WhenAllOnCompleted(this IEnumerable<IObservable<Unit>> sources)
+		{
+			return new WhenAllOnCompleted(sources);
+		}
+	}
+
+	partial class WhenAllOnCompleted : IObservable<Unit>
+	{
+		readonly IEnumerable<IObservable<Unit>> sources;
+
+		public WhenAllOnCompleted(IEnumerable<IObservable<Unit>> sources)
+		{
+			this.sources = sources;
+		}
+
+		public IDisposable Subscribe(IObserver<Unit> observer)
+		{
+			return sources.WhenAll().Subscribe(observer);
+		}
+	}
+}
