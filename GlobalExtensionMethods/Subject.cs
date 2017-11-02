f85d6bab8261555bafe94bd41ba4e752026057c6
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-11-02 17:29:33 +0900

    GlobalExtensionMethods/Subject

---
+++ GlobalExtensionMethods/Subject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UniRx;
+
+public static partial class GlobalExtensionMethods
+{
+	public static void OnNext<T>(this Subject<T> subject, Func<T> factory)
+	{
+		if (subject.HasObservers) subject.OnNext(factory());
+	}
+}
