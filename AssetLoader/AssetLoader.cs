e20a3b4ab1d3808e471e3e5a4d1bfdac4e10d6b3
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-11-08 16:43:23 +0900

    AssetLoader

--- AssetLoader/AssetLoader.cs
+++ AssetLoader/AssetLoader.cs
@@ -4,40 +4,41 @@
 	using UniRx;
 	using UnityEngine;
 	using static J.AssetLoaderInstance;
+	using static J.SingletonMonoBehaviour<AssetLoaderInstance>;
 	using Object = UnityEngine.Object;
 
 	public static class AssetLoader
 	{
-		public static string InspectorManifestUri => AssetLoaderInstance.Instance.InspectorManifestUri;
+		public static string InspectorManifestUri => Instance.InspectorManifestUri;
 
-		public static AssetBundleManifest Manifest => AssetLoaderInstance.Instance.Manifest;
+		public static AssetBundleManifest Manifest => Instance.Manifest;
 
-		public static string RootUri => AssetLoaderInstance.Instance.RootUri;
+		public static string RootUri => Instance.RootUri;
 
 		public static void LoadManifest(string uri) =>
-			AssetLoaderInstance.Instance.LoadManifest(uri);
+			Instance.LoadManifest(uri);
 
 		public static IObservable<Unit> WaitForManifestLoaded() =>
-			AssetLoaderInstance.Instance.WaitForManifestLoaded();
+			Instance.WaitForManifestLoaded();
 
 		public static IObservable<AssetBundle> GetAssetBundle(string bundleName) =>
-			AssetLoaderInstance.Instance.GetAssetBundle(new BundleEntry(bundleName));
+			Instance.GetAssetBundle(new BundleEntry(bundleName));
 
 		public static IObservable<AssetBundle> GetAssetBundleWithDependencies(string bundleName) =>
-			AssetLoaderInstance.Instance.GetAssetBundleWithDependencies(new BundleEntry(bundleName));
+			Instance.GetAssetBundleWithDependencies(new BundleEntry(bundleName));
 
 		public static IObservable<Object> Load(string bundleName, string assetName = null, Type assetType = null) =>
-			AssetLoaderInstance.Instance.Load(new AssetEntry(bundleName, assetName, assetType, LoadMethod.Single));
+			Instance.Load(new AssetEntry(bundleName, assetName, assetType, LoadMethod.Single));
 		public static IObservable<Object> Load(string bundleName, Type assetType) =>
-			AssetLoaderInstance.Instance.Load(new AssetEntry(bundleName, null, assetType, LoadMethod.Single));
+			Instance.Load(new AssetEntry(bundleName, null, assetType, LoadMethod.Single));
 		public static IObservable<T> Load<T>(string bundleName, string assetName = null) where T : Object =>
-			AssetLoaderInstance.Instance.Load(new AssetEntry(bundleName, assetName, typeof(T), LoadMethod.Single)).Select(obj => obj as T);
+			Instance.Load(new AssetEntry(bundleName, assetName, typeof(T), LoadMethod.Single)).Select(obj => obj as T);
 
 		public static IObservable<Object> LoadMulti(string bundleName, string assetName = null, Type assetType = null) =>
-			AssetLoaderInstance.Instance.Load(new AssetEntry(bundleName, assetName, assetType, LoadMethod.Multi));
+			Instance.Load(new AssetEntry(bundleName, assetName, assetType, LoadMethod.Multi));
 		public static IObservable<Object> LoadMulti(string bundleName, Type assetType) =>
-			AssetLoaderInstance.Instance.Load(new AssetEntry(bundleName, null, assetType, LoadMethod.Multi));
+			Instance.Load(new AssetEntry(bundleName, null, assetType, LoadMethod.Multi));
 		public static IObservable<T> LoadMulti<T>(string bundleName, string assetName = null) where T : Object =>
-			AssetLoaderInstance.Instance.Load(new AssetEntry(bundleName, assetName, typeof(T), LoadMethod.Multi)).Select(obj => obj as T);
+			Instance.Load(new AssetEntry(bundleName, assetName, typeof(T), LoadMethod.Multi)).Select(obj => obj as T);
 	}
 }
