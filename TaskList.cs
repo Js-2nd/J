be0ae1db47d1482ef2578df2a43fcc26b9b41377
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-11-30 18:16:39 +0900

    TaskList

---
+++ TaskList.cs
@@ -0,0 +1,29 @@
+﻿namespace J
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UniRx;
+
+	public class TaskList : List<Func<DividableProgress, IObservable<Unit>>>
+	{
+		public void Add(TaskList taskList) => Add(taskList.Start);
+
+		public IObservable<Unit> Start(DividableProgress progress = null)
+		{
+			if (Count == 0)
+			{
+				progress?.Report(1f);
+				return Observable.ReturnUnit();
+			}
+
+			if (Count == 1)
+			{
+				return this[0](progress);
+			}
+
+			float weight = 1f / Count;
+			return this.Select(task => task(progress?.Divide(weight))).WhenAll();
+		}
+	}
+}
