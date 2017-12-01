dd72cb59d48ce49c51e298d8b3dc591e30b8d11e
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-12-01 12:18:58 +0900

    TaskList

--- TaskList.cs
+++ TaskList.cs
@@ -8,32 +8,38 @@
 
 	public class TaskList
 	{
-		List<Func<DividableProgress, IObservable<Unit>>> list = new List<Func<DividableProgress, IObservable<Unit>>>();
+		readonly List<Func<DividableProgress, IObservable<Unit>>> list = new List<Func<DividableProgress, IObservable<Unit>>>();
 
-		public void AddObservable<T>(IObservable<T> observable) => list.Add(p => observable.Finally(() => p?.Report(1f)).AsUnitObservable());
+		public int Count => list.Count;
+
+		public void Clear() => list.Clear();
+
+		public void AddObservable(Func<DividableProgress, IObservable<Unit>> observable) => list.Add(observable);
 
 		public void AddObservable(IObservable<Unit> observable) => list.Add(p => observable.Finally(() => p?.Report(1f)));
 
-		public void AddObservable(Func<DividableProgress, IObservable<Unit>> observable) => list.Add(observable);
+		public void AddObservable<T>(IObservable<T> observable) => list.Add(p => observable.Finally(() => p?.Report(1f)).AsUnitObservable());
 
 		public void AddCoroutine(Func<DividableProgress, IEnumerator> coroutine) => list.Add(p => coroutine(p).ToObservable());
 
 		public void AddTaskList(TaskList taskList) => list.Add(taskList.Start);
 
+		public void MergeTaskList(TaskList taskList) => list.AddRange(taskList.list);
+
 		public IObservable<Unit> Start(DividableProgress progress = null)
 		{
-			if (list.Count == 0)
+			if (Count == 0)
 			{
 				progress?.Report(1f);
 				return Observable.ReturnUnit();
 			}
 
-			if (list.Count == 1)
+			if (Count == 1)
 			{
 				return list[0](progress);
 			}
 
-			float weight = 1f / list.Count;
+			float weight = 1f / Count;
 			return list.Select(task => task(progress?.Divide(weight))).WhenAll();
 		}
 	}
