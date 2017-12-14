abda528334d60f51839aee1275cfc7c78cdfb3f1
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2017-12-14 12:04:00 +0900

    ExtensionMethods/Flag

--- ExtensionMethods/Flag.cs
+++ ExtensionMethods/Flag.cs
@@ -2,24 +2,14 @@
 {
 	public static partial class ExtensionMethods
 	{
-		public static bool HasFlag(this int @this, int flag)
-		{
-			return (@this & flag) == flag;
-		}
+		public static bool HasFlag(this int @this, int flag) => (@this & flag) == flag;
+		public static int SetFlag(this int @this, int flag) => @this | flag;
+		public static int UnsetFlag(this int @this, int flag) => @this & ~flag;
+		public static int FlipFlag(this int @this, int flag) => @this ^ flag;
 
-		public static int SetFlag(this int @this, int flag)
-		{
-			return @this | flag;
-		}
-
-		public static int UnsetFlag(this int @this, int flag)
-		{
-			return @this & ~flag;
-		}
-
-		public static int FlipFlag(this int @this, int flag)
-		{
-			return @this ^ flag;
-		}
+		public static bool HasFlag(this long @this, long flag) => (@this & flag) == flag;
+		public static long SetFlag(this long @this, long flag) => @this | flag;
+		public static long UnsetFlag(this long @this, long flag) => @this & ~flag;
+		public static long FlipFlag(this long @this, long flag) => @this ^ flag;
 	}
 }
