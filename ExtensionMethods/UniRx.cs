8dbe01aff17ce1ae0f3f6db3a4cca99ed64b7560
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2018-05-31 17:52:53 +0900

    delete TaskList

--- ExtensionMethods/UniRx.cs
+++ ExtensionMethods/UniRx.cs
@@ -1,8 +1,6 @@
 ﻿namespace UniRx
 {
-	using J;
 	using System;
-	using System.Collections.Generic;
 
 	public static partial class ExtensionMethods
 	{
@@ -22,22 +20,6 @@
 		public static void OnNext<T>(this Subject<T> subject, Func<T> factory)
 		{
 			if (subject.HasObservers) subject.OnNext(factory());
-		}
-
-		public static TaskList ToTaskList<T>(this IEnumerable<IObservable<T>> sources)
-		{
-			var list = new TaskList();
-			foreach (var source in sources)
-				list.AddObservable(source);
-			return list;
-		}
-
-		public static TaskQueue ToTaskQueue<T>(this IEnumerable<IObservable<T>> sources)
-		{
-			var queue = new TaskQueue();
-			foreach (var source in sources)
-				queue.AddObservable(source);
-			return queue;
 		}
 	}
 }
