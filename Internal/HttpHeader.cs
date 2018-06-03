5c40e06d402567e81f33e20d01d5603a26df892b
Js <JimmyLoveSiren@users.noreply.github.com>
2018-06-03 23:17:48 +0900

    HttpHeader

---
+++ Internal/HttpHeader.cs
@@ -0,0 +1,9 @@
+﻿namespace J.Internal
+{
+	public static class HttpHeader
+	{
+		public const string ContentLength = "Content-Length";
+		public const string ETag = "ETag";
+		public const string IfNoneMatch = "If-None-Match";
+	}
+}
