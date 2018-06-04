fdb12514bdca98e310de40868bf353b8568f75e3
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2018-06-04 11:35:15 +0900

    Exception.Throw<T>

--- GlobalExtensionMethods/Exception.cs
+++ GlobalExtensionMethods/Exception.cs
@@ -7,4 +7,12 @@
 #endif
 		throw exception;
 	}
+
+	public static T Throw<T>(this System.Exception exception)
+	{
+#if NET_4_6
+		System.Runtime.ExceptionServices.ExceptionDispatchInfo.Capture(exception).Throw();
+#endif
+		throw exception;
+	}
 }
