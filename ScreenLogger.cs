418df3288aebf38512ea68c6562102bc03a76c80
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2018-06-12 17:13:14 +0900

    ScreenLogger

--- ScreenLogger.cs
+++ ScreenLogger.cs
@@ -1,11 +1,12 @@
 ﻿namespace J
 {
+	using System;
 	using System.Collections.Generic;
 	using UnityEngine;
 
 	public class ScreenLogger : SingletonMonoBehaviour<ScreenLogger>
 	{
-		public float LogExpiration = 10;
+		public float LogExpire = 10;
 		public int LogCapacity = 200;
 
 		LinkedList<Log> list;
@@ -28,9 +29,8 @@
 		int logIdCount;
 		void OnLogReceived(string message, string stackTrace, LogType type)
 		{
-			var log = new Log(++logIdCount,
-				message, stackTrace, type,
-				Time.realtimeSinceStartup + LogExpiration);
+			var expire = LogExpire > 0 ? Time.realtimeSinceStartup + LogExpire : (float?)null;
+			var log = new Log(++logIdCount, message, stackTrace, type, expire);
 			var node = list.AddLast(log);
 			dict.Add(log.Id, node);
 			if (list.Count > LogCapacity)
@@ -46,8 +46,37 @@
 			dict.Clear();
 		}
 
+		void Update() // TODO
+		{
+			if (Input.GetMouseButtonDown(0)) Debug.Log("This is a log message.");
+			if (Input.GetMouseButtonDown(1)) Debug.LogWarning("This is a warning message.");
+			if (Input.GetMouseButtonDown(2)) Debug.LogError("This is an error message.");
+		}
+
+		LinkedListNode<Log> startNode;
 		void OnGUI() // TODO
 		{
+			if (list.Count <= 0) return;
+			ScaleGUI();
+			using (new State())
+			{
+				if (startNode?.List == null) startNode = list.First;
+				while (startNode?.Value.Expire < Time.realtimeSinceStartup)
+					startNode = startNode.Next;
+				for (var node = startNode; node != null; node = node.Next)
+				{
+					var log = node.Value;
+					GUI.contentColor = LogTypeColor(log.Type);
+					GUILayout.Box(log.Message);
+				}
+			}
+		}
+
+		static void ScaleGUI()
+		{
+			float scale = Mathf.Max(Screen.width, Screen.height) / 1920f;
+			scale += Mathf.Min(Screen.width, Screen.height) / 1080f;
+			GUIUtility.ScaleAroundPivot(new Vector2(scale, scale), Vector2.zero);
 		}
 
 		static Color LogTypeColor(LogType type)
@@ -62,21 +91,36 @@
 			}
 		}
 
-		public class Log
+		class Log
 		{
 			public readonly int Id;
 			public readonly string Message;
 			public readonly string StackTrace;
 			public readonly LogType Type;
-			public readonly float Expiration;
+			public readonly float? Expire;
 
-			public Log(int id, string message, string stackTrace, LogType type, float expiration)
+			public Log(int id, string message, string stackTrace, LogType type, float? expire)
 			{
 				Id = id;
 				Message = $"<{Id}> {message}";
 				StackTrace = !string.IsNullOrWhiteSpace(stackTrace) ? stackTrace : null;
 				Type = type;
-				Expiration = expiration;
+				Expire = expire;
+			}
+		}
+
+		class State : IDisposable
+		{
+			readonly Color contentColor;
+
+			public State()
+			{
+				contentColor = GUI.contentColor;
+			}
+
+			public void Dispose()
+			{
+				GUI.contentColor = contentColor;
 			}
 		}
 	}
