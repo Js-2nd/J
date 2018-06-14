bffedd80021cf9e793681bd4f5fe31ae68bd7067
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2018-06-14 15:28:24 +0900

    RenderTexture.Clear

--- GlobalExtensionMethods/Unity.cs
+++ GlobalExtensionMethods/Unity.cs
@@ -1,11 +1,21 @@
-﻿public static partial class GlobalExtensionMethods
+﻿using UnityEngine;
+
+public static partial class GlobalExtensionMethods
 {
-	public static T GetOrAddComponent<T>(this UnityEngine.GameObject gameObject) where T : UnityEngine.Component
+	public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
 	{
 		T component = gameObject.GetComponent<T>();
 		if (component != null) return component;
 		return gameObject.AddComponent<T>();
 	}
 
-	public static T GetOrAddComponent<T>(this UnityEngine.Component component) where T : UnityEngine.Component => component.gameObject.GetOrAddComponent<T>();
+	public static T GetOrAddComponent<T>(this Component component) where T : Component => component.gameObject.GetOrAddComponent<T>();
+
+	public static void Clear(this RenderTexture rt)
+	{
+		var last = RenderTexture.active;
+		RenderTexture.active = rt;
+		GL.Clear(true, true, Color.clear);
+		RenderTexture.active = last;
+	}
 }
