1f90aa91d23780848ef5051d535b0cfc1ef52d92
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2018-06-20 23:55:41 +0900

    Unity.Extensions

---
+++ ExtensionMethods/Unity.Extensions.cs
@@ -0,0 +1,95 @@
+﻿namespace J.Extensions
+{
+	using UnityEngine;
+
+	public static partial class ExtensionMethods
+	{
+		public static T SetPosition<T>(this T tr, Vector3 pos) where T : Transform
+		{
+			tr.position = pos;
+			return tr;
+		}
+		public static T SetPosition<T>(this T tr, float? x = null,
+			float? y = null, float? z = null) where T : Transform
+		{
+			var pos = tr.position;
+			if (x.HasValue) pos.x = x.Value;
+			if (y.HasValue) pos.y = y.Value;
+			if (z.HasValue) pos.z = z.Value;
+			tr.position = pos;
+			return tr;
+		}
+		public static T SetLocalPosition<T>(this T tr, Vector3 pos) where T : Transform
+		{
+			tr.localPosition = pos;
+			return tr;
+		}
+		public static T SetLocalPosition<T>(this T tr, float? x = null,
+			float? y = null, float? z = null) where T : Transform
+		{
+			var pos = tr.localPosition;
+			if (x.HasValue) pos.x = x.Value;
+			if (y.HasValue) pos.y = y.Value;
+			if (z.HasValue) pos.z = z.Value;
+			tr.localPosition = pos;
+			return tr;
+		}
+
+		public static T SetRotation<T>(this T tr, Quaternion rot) where T : Transform
+		{
+			tr.rotation = rot;
+			return tr;
+		}
+		public static T SetLocalRotation<T>(this T tr, Quaternion rot) where T : Transform
+		{
+			tr.localRotation = rot;
+			return tr;
+		}
+
+		public static T SetEulerAngles<T>(this T tr, Vector3 angles) where T : Transform
+		{
+			tr.eulerAngles = angles;
+			return tr;
+		}
+		public static T SetEulerAngles<T>(this T tr, float? x = null, float? y = null, float? z = null) where T : Transform
+		{
+			var angles = tr.eulerAngles;
+			if (x.HasValue) angles.x = x.Value;
+			if (y.HasValue) angles.y = y.Value;
+			if (z.HasValue) angles.z = z.Value;
+			tr.eulerAngles = angles;
+			return tr;
+		}
+		public static T SetLocalEulerAngles<T>(this T tr, Vector3 angles) where T : Transform
+		{
+			tr.localEulerAngles = angles;
+			return tr;
+		}
+		public static T SetLocalEulerAngles<T>(this T tr, float? x = null, float? y = null, float? z = null) where T : Transform
+		{
+			var angles = tr.localEulerAngles;
+			if (x.HasValue) angles.x = x.Value;
+			if (y.HasValue) angles.y = y.Value;
+			if (z.HasValue) angles.z = z.Value;
+			tr.localEulerAngles = angles;
+			return tr;
+		}
+
+		public static T SetLocalScale<T>(this T tr, Vector3 scale) where T : Transform
+		{
+			tr.localScale = scale;
+			return tr;
+		}
+		public static T SetLocalScale<T>(this T tr, float? x = null, float? y = null, float? z = null) where T : Transform
+		{
+			var scale = tr.localScale;
+			if (x.HasValue) scale.x = x.Value;
+			if (y.HasValue) scale.y = y.Value;
+			if (z.HasValue) scale.z = z.Value;
+			tr.localScale = scale;
+			return tr;
+		}
+
+		public static Canvas GetCanvas(this RectTransform rt) => rt.GetComponentInParent<Canvas>();
+	}
+}
