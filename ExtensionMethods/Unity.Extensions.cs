475705c99940159fd2e76dbbf6d9a211ab4715f2
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2018-06-21 00:34:04 +0900

    Unity.Extensions

--- ExtensionMethods/Unity.Extensions.cs
+++ ExtensionMethods/Unity.Extensions.cs
@@ -91,5 +91,29 @@
 		}
 
 		public static Canvas GetCanvas(this RectTransform rt) => rt.GetComponentInParent<Canvas>();
+
+		public static Rect GetScreenRect(this RectTransform rt)
+		{
+			var min = rt.TransformPoint(rt.rect.min);
+			var max = rt.TransformPoint(rt.rect.max);
+			return new Rect(min, max - min);
+		}
+
+		public static RectTransform SetScreenRect(this RectTransform rt, Rect rect) =>
+			rt.SetScreenRect(rect, rt.GetCanvas().rootCanvas.worldCamera);
+		public static RectTransform SetScreenRect(this RectTransform rt, Rect rect, Camera cameraHint)
+		{
+			Vector3 min, max;
+			if (RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, rect.min, cameraHint, out min) &&
+				RectTransformUtility.ScreenPointToWorldPointInRectangle(rt, rect.max, cameraHint, out max))
+			{
+				rt.anchorMin = Vector2.zero;
+				rt.anchorMax = Vector2.zero;
+				rt.pivot = Vector2.zero;
+				rt.sizeDelta = max - min;
+				rt.position = min;
+			}
+			return rt;
+		}
 	}
 }
