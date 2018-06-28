3a2b36923f7c43633d7ea4c6f3b5c1fde0b0dd0c
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2018-06-28 16:29:45 +0900

    HttpUtil

--- Utilities/HttpUtil.cs
+++ Utilities/HttpUtil.cs
@@ -2,24 +2,37 @@
 {
 	using J.Internal;
 	using System;
+	using System.IO;
 	using UniRx;
 	using UnityEngine;
 	using UnityEngine.Networking;
 
 	public static class HttpUtil
 	{
-		public static IObservable<ulong?> GetContentLength(string uri, IProgress<float> progress = null)
+		public static IObservable<ulong?> GetContentLength(string url, IProgress<float> progress = null) =>
+			GetContentLength(url, null, progress);
+		public static IObservable<ulong?> GetContentLength(string url, string eTag, IProgress<float> progress = null)
 		{
 			return Observable.Defer(() =>
 			{
-				return UnityWebRequest.Head(uri).SendAsObservable(progress).Select(req =>
+				var req = UnityWebRequest.Head(url);
+				if (eTag != null) req.SetRequestHeader(HttpHeader.IfNoneMatch, eTag);
+				return req.SendAsObservable(progress, false, false).Select(r =>
 				{
+					if (r.responseCode != 304) r.TryThrowError();
 					ulong length;
-					if (ulong.TryParse(req.GetResponseHeader(HttpHeader.ContentLength), out length))
+					if (ulong.TryParse(r.GetResponseHeader(HttpHeader.ContentLength), out length))
 						return length;
 					return (ulong?)null;
 				});
 			});
+		}
+
+		public static IObservable<UnityWebRequest> DownloadToPersist(string url, string path)
+		{
+			string fullPath = Path.Combine(Application.persistentDataPath, path);
+			var req = new UnityWebRequest(url) { downloadHandler = new DownloadHandlerFile(fullPath) };
+			return req.SendAsObservable();
 		}
 	}
 }
