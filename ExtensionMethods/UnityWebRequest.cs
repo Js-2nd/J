0ffc42c2aa2ea2734440402b3ffa5bd6d3af1a49
JimmyLoveSiren <JimmyLoveSiren@users.noreply.github.com>
2018-07-20 14:34:57 +0900

    UnityWebRequest.SendAsObservable

--- ExtensionMethods/UnityWebRequest.cs
+++ ExtensionMethods/UnityWebRequest.cs
@@ -9,38 +9,36 @@
 	public static partial class ExtensionMethods
 	{
 		public static IObservable<UnityWebRequest> SendAsObservable(this UnityWebRequest request,
-			IUnityWebRequestSendOptions options = null, IProgress<float> progress = null) => Observable.Defer(() =>
+			IProgress<float> progress = null, string eTag = null, string lastModified = null,
+			bool throwNetworkError = true, bool throwHttpError = true,
+			bool autoDispose = true) => Observable.Defer(() =>
 		{
 			if (request == null) throw new ArgumentNullException(nameof(request));
-			if (options == null) options = new UnityWebRequestSendOptions();
-			if (options.ETag != null) request.SetIfNoneMatch(options.ETag);
-			if (options.LastModified != null) request.SetIfModifiedSince(options.LastModified);
-			var stream = request.SendWebRequest()
-				.AsAsyncOperationObservable(progress)
-				.Select(op =>
+			if (eTag != null) request.SetIfNoneMatch(eTag);
+			if (lastModified != null) request.SetIfModifiedSince(lastModified);
+			var stream = request.SendWebRequest().AsAsyncOperationObservable(progress).Select(op =>
+			{
+				var req = op.webRequest;
+				if (req.responseCode != 304)
 				{
-					var req = op.webRequest;
-					if (req.responseCode != 304)
-					{
-						if (req.isNetworkError && options.ThrowNetworkError) throw new NetworkException(req);
-						if (req.isHttpError && options.ThrowHttpError) throw new HttpException(req);
-					}
-					return req;
-				});
-			if (options.AutoDispose) stream = stream.Finally(request.Dispose);
+					if (req.isNetworkError && throwNetworkError) throw new NetworkException(req);
+					if (req.isHttpError && throwHttpError) throw new HttpException(req);
+				}
+				return req;
+			});
+			if (autoDispose) stream = stream.Finally(request.Dispose);
 			return stream;
 		});
 
-		public static IObservable<AssetBundle> LoadAssetBundle(this IObservable<UnityWebRequest> source,
-			bool throwError = true)
+		public static IObservable<AssetBundle> LoadAssetBundle(
+			this IObservable<UnityWebRequest> source, bool throwError = true)
 		{
 			return source.Select(request =>
 			{
 				try
 				{
 					var bundle = DownloadHandlerAssetBundle.GetContent(request);
-					if (bundle == null)
-						throw new InvalidDataException("Invalid AssetBundle. " + request.url);
+					if (bundle == null) throw new InvalidDataException("Invalid AssetBundle. " + request.url);
 					return bundle;
 				}
 				catch
