d56f61885c8cb5ef0fc85c621d0fd08382ec776f
Js <JimmyLoveSiren@users.noreply.github.com>
2018-10-28 13:41:48 +0900

    PriorityLinkedList

--- WIP/PriorityLinkedList.cs
+++ WIP/PriorityLinkedList.cs
@@ -7,14 +7,14 @@ namespace J
 
 	public class PriorityLinkedList<T> : IReadOnlyCollection<T>
 	{
-		readonly SortedDictionary<int, HashedLinkedList<T>> main;
+		readonly TreeDictionary<int, HashedLinkedList<T>> main;
 		readonly HashDictionary<T, int> sub;
 		readonly IEqualityComparer<T> cmp;
 
 		public PriorityLinkedList() : this(EqualityComparer<T>.Default) { }
 		public PriorityLinkedList(IEqualityComparer<T> comparer)
 		{
-			main = new SortedDictionary<int, HashedLinkedList<T>>();
+			main = new TreeDictionary<int, HashedLinkedList<T>>();
 			sub = new HashDictionary<T, int>(comparer);
 			cmp = comparer;
 		}
@@ -24,11 +24,15 @@ namespace J
 
 		public bool Contains(T item) => sub.Contains(item);
 
+		HashedLinkedList<T> reserveList;
 		public void Add(T item, int priority = 0)
 		{
 			int oldPriority;
 			if (sub.UpdateOrAdd(item, priority, out oldPriority)) RemoveMain(oldPriority, item);
-			main.GetOrAdd(priority, _ => new HashedLinkedList<T>(cmp)).Add(item);
+			if (reserveList == null) reserveList = new HashedLinkedList<T>(cmp);
+			var list = reserveList;
+			if (!main.FindOrAdd(priority, ref list)) reserveList = null;
+			list.Add(item);
 		}
 
 		public bool Remove(T item)
@@ -50,8 +54,9 @@ namespace J
 
 		T RemoveMain(int priority, T item)
 		{
-			var list = main.GetOrDefault(priority);
-			if (list == null) return default(T);
+			int p = priority;
+			HashedLinkedList<T> list;
+			if (!main.Find(ref p, out list)) return default(T);
 			T removedItem;
 			list.Remove(item, out removedItem);
 			if (list.Count == 0) main.Remove(priority);
